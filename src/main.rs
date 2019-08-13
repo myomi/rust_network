9ddc93d3d38c9d6d715e9e6a8494c3b50cabd5dc
MITSUYA MYOUCHIN <m.myouchin@nakamura-joho.co.jp>
2019-08-13 02:36:54 +0000

    handle arguments

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,53 @@
 use env_logger;
-use log::{debug, error, info, warn};
+use log::{error, info};
 use std::env;
 
 fn main() {
-    env::set_var("RUST_LOG", "DEBUG");
+    // setup logger
+    env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
-    info!("Hello, logger");
-    error!("error");
-    warn!("warning!");
-    debug!("debug!!");
+    // arguments
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 4 {
+        error!("Please specify [tcp|udp] [server|client] [addr:port].");
+        std::process::exit(1);
+    }
+    let protocol: &str = &args[1];
+    let role: &str = &args[2];
+    let address = &args[3];
+    info!("protocol: {}", protocol);
+    info!("role: {}", role);
+    info!("address: {}", address);
+
+    // startup
+    match (protocol, role) {
+        ("tcp", "server") => {
+            unimplemented!();
+        },
+        ("tcp", "client") => {
+            unimplemented!();
+        },
+        ("tcp", _) => {
+            missing_role();
+        },
+        ("udp", "server") => {
+            unimplemented!();
+        },
+        ("udp", "client") => {
+            unimplemented!();
+        },
+        ("udp", _) => {
+            missing_role();
+        },
+        _ => {
+            error!("Please specify tcp or udp on the 1st argument.");
+            std::process::exit(1);
+        }
+    }
+}
+
+fn missing_role() {
+    error!("Please specify server or client on the 2nd argument.");
+    std::process::exit(1);
 }
